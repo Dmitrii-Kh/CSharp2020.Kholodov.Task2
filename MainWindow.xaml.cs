062802878c9cde575afeec9d7896016d9370d18d
DK <novayapochta123@gmail.com>
2020-03-12 19:45:53 +0200

    first one

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using CSharpProgramming2020.Task2.VIewModels;
+
+
+namespace CSharpProgramming2020.Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new UserViewModel();
+        }
+    }
+}
---
+++ UserView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using CSharpProgramming2020.Task2.VIewModels;
+
+namespace CSharpProgramming2020.Task2
+{
+    /// <summary>
+    /// Логика взаимодействия для UserView.xaml
+    /// </summary>
+    public partial class UserView : UserControl
+    {
+        private UserViewModel _viewModel;
+        public UserView()
+        {
+            InitializeComponent();
+            DataContext = _viewModel = new UserViewModel();
+        }
+    }
+}
